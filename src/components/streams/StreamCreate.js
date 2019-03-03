33fb7c0a8a5575877949452ee393dcab7a548f9f
federicoviscomi <f@neterea.com>
2019-03-03 15:16:36 +0000

    section 19 end

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const StreamCreate = () => {
-    return (
-        <div>StreamCreate</div>
-    );
+class StreamCreate extends Component {
+
+    renderError({error, touched}) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = ({input, label, meta}) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+        return (
+            <div
+                className="field">
+                <label>
+                    {label}
+                </label>
+                <input
+                    autoComplete="off"
+                    {...input}
+                />
+                {this.renderError(meta)}
+            </div>
+        );
+    };
+
+    onSubmit(formValues) {
+        const errors = {};
+        if (!formValues.title) {
+            errors.title = "you must enter a title";
+        }
+        if (!formValues.description) {
+            errors.description = 'you must enter a description';
+        }
+        return errors;
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field
+                    name="title"
+                    component={this.renderInput}
+                    label="Enter description"
+                />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                />
+                <button
+                    className="ui button primary">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+const validate = (formValues) => {
+
 };
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
+import {reducer} from 'redux-form';
 
-export default combineReducers({auth: () => authReducer});
+export default combineReducers({
+    auth: () => authReducer,
+    form: reducer
+});
